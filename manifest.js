3e1007f1599fcb5c1eb31fc731131b6516196ff8
Jonas Almeida <jalmeida@uab.edu>
2013-02-13 02:15:25 -0600

    some wApp added to the demo

--- manifest.js
+++ manifest.js
@@ -1,28 +1,5 @@
 
 wApps.manifest.apps.push(
-    {
-    "name": "Login@ S3DB.UAB",
-    "description": "<p>Very simple login into UAB's S3DB cloud deployment using jmat's toolbox.</p><p>The login credentials will be stored at wApps.s3db.info.</p>",
-    "url": "https://code.google.com/p/jmat/", // home page of App
-    "namespace":'jmat',
-    buildUI:function(id){
-        this.require('http://localhost:8888/jmat/jmat.js',
-            function(){
-                $('<div id="'+id+'_s3dbLogin">').appendTo($('#'+id));// create login div
-                var url = 'https://uab.s3db.org/s3db'
-                jmat.s3db.UI.login(
-                    url,
-                    function(){
-                        wApps.s3db=jmat.s3db.info;
-                        console.log('s3db login successful :-)');
-                        $('<div>Logged in at '+url+': <p>'+JSON.stringify(wApps.s3db.uid)+'</p>').appendTo($('#'+id));
-                    },
-                    id+'_s3dbLogin'
-                )
-                //console.log(id);
-            });
-        }
-    },
 
     {
     "name": "QMachine",
@@ -33,6 +10,44 @@ wApps.manifest.apps.push(
         this.require('', // script needed to volunteer compute cycles to QM
             function(){
                 $('#'+id).html("<h1>QMachine</h1>Volunteering to Sean's QMachine will go here, Sean, can you please change the manifest accordingly?");
+            });
+        }
+    },
+
+
+    {
+    "name": "Login@ S3DB.UAB",
+    "description": "<p>Very simple login into UAB's S3DB cloud deployment using jmat's toolbox.<br> The login credentials will be stored at wApps.s3db.info.",
+    "url": "https://code.google.com/p/jmat/", // home page of App
+    "namespace":'jmat',
+    buildUI:function(id){
+        this.require('https://jmat.googlecode.com/git/jmat.js',
+            function(){
+                $('<div id="'+id+'_s3dbLogin">').appendTo($('#'+id));// create login div
+                var url = 'https://uab.s3db.org/s3db'
+                jmat.s3db.UI.login(
+                    url,
+                    function(){
+                        wApps.s3db=jmat.s3db.info;
+                        console.log('s3db login successful :-)');
+                        $('<div>Logged in at '+url+': <p><smal><em>'+JSON.stringify(wApps.s3db.uid)+'</em></small></p>').appendTo($('#'+id));
+                    },
+                    id+'_s3dbLogin'
+                )
+                //console.log(id);
+            });
+        }
+    },
+
+    {
+    "name": "someWApp",
+    "description": "Some wApp one of you links here",
+    "url": "http://uab.mathbiol.org/workshop",
+    "namespace":'Some_wAapp',
+    buildUI:function(id){
+        this.require('', // script to load your code 
+            function(){
+                $('#'+id).html("<h1>Some wApp</h1>Some Application you developped and want to wApp here");
             });
         }
     }
